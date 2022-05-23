b3fded0978ab2022dd5518aaeb197e5c22f484fb
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-23 11:21:49 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Task 6 to print 0-9 differently
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+putchar('\n');
+
+return (0);
+}
