4ce3f9034aff8442cdc73e75ae64cbf1157d5bcf
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-23 12:08:52 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Task 9 to print all single-digit numbers with a comma to separate
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != '9')
+		{
+			putchar(x);
+			putchar(',');
+			putchar(' ');
+		} else
+		{
+			putchar(x);
+		}
+	}
+
+return (0);
+}
