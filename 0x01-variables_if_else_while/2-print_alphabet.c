bf63cd3f9000ac39684a32ceaaa25569b9986548
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-23 10:01:29 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Task 2 to play alphabet game
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+return (0);
+}
