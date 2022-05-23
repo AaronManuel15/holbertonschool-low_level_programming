07f90f226c4a6751e9272208451b5e2d446eba8b
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-23 11:09:55 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Task 5 to print 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d\n", x);
+	}
+
+return (0);
+}
