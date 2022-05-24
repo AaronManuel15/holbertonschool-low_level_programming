81ec586ec7e04c68c6a7f92793419042a6fbea9c
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-24 12:17:28 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs(int n) - Task 6 program prints the abs val of int
+ *
+ * @n: Input from user or program
+ *
+ * Return: 0 (Successful)
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		_putchar(n);
+		return (0);
+	} else
+	{
+		n *= -1
+		_putchar(n);
+		return (0);
+	}
+return (0);
+}
