7fa25ed00254088c36cf70edf6814d2d09f54129
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-24 15:26:44 -0700

    betty stuff

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,17 +3,19 @@
 /**
  * print_to_98 - Task 11 prints all natural numbers from n to 98
  *
+ * @n: given from user/program
+ *
  * Return: 0 (Successful)
  *
  */
 void print_to_98(int n)
 {
 	int i = n;
+
 	if (n < 98)
 	{
 		for (i = n; i < 98; i++)
 		{
-			
 			printf("%d, ", i);
 		}
 	} else if (n > 98)
