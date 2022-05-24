27009947e173d245b66c43cd1c8b84ba3cd07f7d
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-24 13:27:54 -0700

    added modular

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ for (h = 0; h <= 23; h++)
 	for (m = 0; m <= 59; m++)
 	{
 		_putchar('0' + (h / 10));
-		_putchar('0' + (h / 10));
+		_putchar('0' + (h % 10));
 		_putchar(':');
 		_putchar('0' + (m / 10));
 		_putchar('0' + (m % 10));
