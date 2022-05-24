0338afc0792991de04e776dd7d45e27303605a6e
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-24 13:25:06 -0700

    moved all putchars

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,14 +12,14 @@ int m;
 
 for (h = 0; h <= 23; h++)
 {
-	_putchar('0' + (h / 10));
-	_putchar('0' + (h % 10));
-}
 	for (m = 0; m <= 59; m++)
 	{
+		_putchar('0' + (h / 10));
+		_putchar('0' + (h / 10));
 		_putchar(':');
 		_putchar('0' + (m / 10));
 		_putchar('0' + (m % 10));
 		_putchar('\n');
 	}
+}
 }
