d6e5c1b6f0675dc357a9ed340d6792dfad890106
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-24 08:20:20 -0700

    Added files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Task 0 program the prints _putchar
+ *
+ * Return: 0 (Successful)
+ *
+ */
+
+int main(void)
+{
+	
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Author: Aaron Manuel
+School: Holberton Tulsa
+
+Purpose: This directory contains the answers/files to 0x02. C - Functions, nested loops
