464c35b2b8a522603f5329e84c1f9370d27c5b7d
AaronManuel15 <aaronmanuel15@gmail.com>
2022-05-28 11:09:15 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Task 8
+ *
+ * @size: input from user for size of square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{	
+	for (j = 1; j <= size; j++)
+		{	
+			for (i = 1; i <= size; i++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
