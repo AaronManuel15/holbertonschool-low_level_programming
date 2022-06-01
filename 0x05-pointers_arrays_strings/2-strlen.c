93138760d52f79fd6ea1460efddd25292abf93de
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-01 11:32:53 -0700

    happy betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strlen - Task 2
+ * _strlen - Task 2
  *
  * @s: given from user to determine it's length
  *
@@ -11,6 +11,8 @@ int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
 	return (i);
 }
