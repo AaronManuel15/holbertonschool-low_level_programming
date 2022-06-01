099f5afdfcc28769145afd55c7df51500fac2167
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-01 13:10:21 -0700

    Task 6 updated with length checker

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,12 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	for (i = 0; i <= len; i += 2)
 	{
 		_putchar(str[i]);
 	}
