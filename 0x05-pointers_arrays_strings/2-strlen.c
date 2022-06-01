1fa017077937cdcc7c8d2ae22c8895477cbe2e52
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-01 11:18:55 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * strlen - Task 2
+ *
+ * @s: given from user to determine it's length
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+	return (i);
+}
