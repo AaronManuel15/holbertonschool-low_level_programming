8e0cd8e6e85edc1946ea98b74885cfe65e381af5
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-01 13:59:51 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - Task 7
+ *
+ * @str: given from user for half to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len;
+	int n;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+		for (i = n; i <= len - 1; i ++)
+		{
+			_putchar(str[i]);
+		}
+	} else
+	{
+		n = (len - 1) / 2;
+		for (i = n; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
