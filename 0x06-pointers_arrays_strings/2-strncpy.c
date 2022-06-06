b040b7054b83f40a2085306ff73667fc0c25c217
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-06 10:51:27 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Task 2
+ *
+ * @dest: destination that string is getting appended to
+ * @src: string to be copied
+ * @n: byte count limit
+ *
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	       dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+return (dest);
+}
