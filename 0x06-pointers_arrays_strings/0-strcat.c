df878c6e072aadea9176a9de09da1c728552d735
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-06 10:06:12 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - Task 0
+ *
+ * @dest: destination that string is getting appended to
+ * @src: string to be copied
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+	
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+return (dest);
+}
