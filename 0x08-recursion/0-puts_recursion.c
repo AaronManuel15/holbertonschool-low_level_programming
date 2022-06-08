d4f135a8bd1f2461c7ecfaa679f2cb9d7da70245
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-08 09:07:37 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Task 0
+ *
+ *@s: string to be printed followed by new line
+ *
+ * Return: void
+ */
+
+ void _puts_recursion(char *s)
+ {
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+ }	
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+Author: Aaron Manuel<br/>
+School: Holberton Tulsa<br/>
+Purpose: This directory holds all the answers/files to 0x08. C - Recursion<br/>
