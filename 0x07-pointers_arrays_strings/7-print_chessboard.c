2b57c6f58687ebb6f38af330aa6880c057c29087
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-07 17:45:21 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _print_chessboard - Task 6
+ *
+ *@a: array that holds chessboard values.
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	for (; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
