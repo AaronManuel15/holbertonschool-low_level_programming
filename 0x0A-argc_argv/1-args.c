90a12bdc133e886c3c15ee2c1ce5cf025f3e84b6
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-14 09:00:27 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Task 1
+ *
+ *@argc: number of arguments passed
+ *@argv: string of arguments passed
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
