3a0d2dc2da6d9ab243ffa191357fac02750ecd47
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-14 11:09:19 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Task 4
+ *
+ *@argc: number of arguments passed
+ *@argv: string of arguments passed
+ *
+ * Return: 0 if successful, 1 if not
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	} else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = 0;
+			for (; argv[i][j]; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
