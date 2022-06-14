42523277d29818c7aa1c8b901629a6a65cf82530
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-14 08:20:54 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Task 0
+ *
+ *@argc: number of arguments passed
+ *@argv: string of arguments passed 
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
