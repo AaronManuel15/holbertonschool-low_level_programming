0a352b0a2b43994db36573f9176dd0096d492cc1
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-21 13:56:00 -0700

    changed for loop condition

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			concatstr[i] = s1[i];
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			concatstr[i] = s2[j];
 			i++;
