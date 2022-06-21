abaf273848648d74c0e08d81b47c2f145648391a
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-21 11:53:37 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *_calloc - Task 2: rebuilding calloc
+  *@nmemb: number of elements 
+  *@size: element size in bytes
+  *
+  *
+  *Return: void
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *memory;
+	size_t i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	memory = malloc(nmemb * size);
+
+	if (memory == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		memory[i] = 0;
+
+	return (memory);
+}
