08108d5fb8d182aafa1d39c4ba863cbf21796103
AaronManuel15 <aaronmanuel15@gmail.com>
2022-07-07 10:30:51 -0700

    moved the struct above the prototypes and fixed some betty issues for task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all of the elements of list_t
+ * @h: pointer given
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
