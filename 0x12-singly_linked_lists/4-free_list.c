b7f6f81bb063dd5895eb3a09ad3180f0662a86ca
AaronManuel15 <aaronmanuel15@gmail.com>
2022-07-08 09:07:13 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list_t
+ * @head: pointer to head of list
+ * 
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptrHolder;
+
+	while(head->next)
+	{
+		ptrHolder = head->next;
+		free(head->str);
+		free(head);
+		head = ptrHolder;
+	}
+	free(head->str);
+	free(head);
+}
