ed3fb18a170394592ad29838b712b0931870b170
AaronManuel15 <aaronmanuel15@gmail.com>
2022-07-13 14:57:32 -0700

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of the bit referred to by index to 1
+ * @index: bit location
+ * @n: integer given
+ *
+ * Return: 1 on success, -1 on error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = *n & ~(1 << index);
+	
+	*n = mask | (1 << index);
+	return (1);
+}
