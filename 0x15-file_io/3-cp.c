a60a59d5dce789ce29a58773b938011a7f17ce9e
AaronManuel15 <aaronmanuel15@gmail.com>
2022-07-18 15:38:46 -0700

    changed Error message

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,14 +24,14 @@ int main(int argc, char *argv[])
 	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 < 0)
-		dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	do {
 		written = read(fd1, buf, 1024);
 		write(fd2, buf, written);
 	} while (written == 1024);
 	if (close(fd1) == -1)
-		dprintf(STDERR_FILENO, "Can't close fd %d", fd1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1), exit(100);
 	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Can't close fd %d", fd2), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2), exit(100);
 	return (0);
 }
