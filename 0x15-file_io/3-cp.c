48c072a050295f3e607cc84377976a8c72eead86
AaronManuel15 <aaronmanuel15@gmail.com>
2022-07-18 15:34:38 -0700

    Betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,12 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
-	if ( fd1 == -1)
+	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	
 	if (fd2 < 0)
 		dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]), exit(99);
 	do {
