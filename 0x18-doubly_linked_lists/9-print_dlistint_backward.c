f3b87b0978fd87b93f9e7a7f7ac25261e1dfbff1
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-02 10:00:37 -0700

    Task 7

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - prints list backwards
+ * @h: pointer to node
+ *
+ * Return: number of nodes
+ *
+ */
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	const dlistint_t *surferPtr = h;
+	int nodeCount = 0;
+
+	while (surferPtr->next != NULL)
+		surferPtr = surferPtr->next;
+
+	while (surferPtr != NULL)
+	{
+		nodeCount++;
+		printf("%d\n", surferPtr->n);
+		surferPtr = surferPtr->prev;
+	}
+	return (nodeCount);
+}
