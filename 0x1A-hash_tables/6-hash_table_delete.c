7d38261a38803bb52c015ab5d2cd46a020d290b0
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-03 17:30:36 -0700

    Task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to be deleted
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *surferPtr;
+
+	if (!ht)
+		return;
+	if (!(ht->array))
+	{
+		free(ht);
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			surferPtr = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array);
+			ht->array[i] = surferPtr;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
