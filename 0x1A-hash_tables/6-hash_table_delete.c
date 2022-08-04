f87cce3f99a44dc64efb7b55f32cd79ee6515be7
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-04 12:34:10 -0700

    added a couple frees

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,6 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *surferPtr;
 
+
 	if (!ht)
 		return;
 	if (!(ht->array))
@@ -19,14 +20,14 @@ void hash_table_delete(hash_table_t *ht)
 		free(ht);
 		return;
 	}
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; i <= ht->size; i++)
 	{
 		while (ht->array[i])
 		{
 			surferPtr = ht->array[i]->next;
 			free(ht->array[i]->key);
 			free(ht->array[i]->value);
-			free(ht->array);
+			free(ht->array[i]);
 			ht->array[i] = surferPtr;
 		}
 	}
