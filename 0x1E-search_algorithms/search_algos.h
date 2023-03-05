1152e275eb8e227cf552c64aed9a88a9c78a62bf
AaronManuel15 <aaronmanuel15@gmail.com>
2023-03-05 00:36:39 -0600

    Task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_chop(int *array, size_t low, size_t high, int value);
 void printArray(int *array, size_t low, size_t high);
+int advanced_binary(int *array, size_t size, int value);
 #endif
